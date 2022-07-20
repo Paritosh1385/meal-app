bcca9129e073baba89bc32804c714b7bf6c60260
paritosh <paritosh1385@gmail.com>
2022-07-20 08:41:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-info">
+        this is meal App.
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+            <div className="header">
+                <div>Home</div>
+                <div>
+                    <img src=""></img>
+                </div>
+            </div>
+    )
+}
